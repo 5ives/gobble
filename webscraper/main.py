51f654f513c397616c5a43c8cbc69cd0695faa37
Lorenzo Paras <ivesparas@gmail.com>
2023-01-18 19:35:11 +1100

    add flags to chrome options to reduce ram

--- webscraper/main.py
+++ webscraper/main.py
@@ -41,6 +41,13 @@ def setupDriver():
     chromeOptions.add_argument('--headless')
     chromeOptions.add_argument('--disable-dev-shm-usage')
     chromeOptions.add_argument('--no-sandbox')
+    chromeOptions.add_argument("start-maximized")
+    chromeOptions.add_argument("disable-infobars")
+    chromeOptions.add_argument("--disable-extensions")
+    chromeOptions.add_argument('--no-sandbox')
+    chromeOptions.add_argument('--disable-application-cache')
+    chromeOptions.add_argument('--disable-gpu')
+    chromeOptions.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(
         ChromeDriverManager().install(),
