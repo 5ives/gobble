cfc808dd4c74b4dc1d619db5c0613512218eeb4c
Lorenzo Paras <ivesparas@gmail.com>
2023-01-25 18:35:25 +1100

    fix ram problem by resetting driver each iteration

--- webscraper/main.py
+++ webscraper/main.py
@@ -9,26 +9,15 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-'''
-Returns the URL to search for certain categories in the Uber Eats website.
-'''
-def getQueryUrl(category):
+location = 'Pitt Street Mall'
+
+def getFullQueryUrl(category):
     return f'https://www.ubereats.com/au/category/sydney-nsw/{category}?mod=seoEnterAddress&next=%2Fau%2Fsearch%3Fq%3D{category}&ps=1'
 
-'''
-This function takes a single argument, entry, which is a dictionary containing a browser log entry. It extracts the
-message field from the entry dictionary, and then parses it as a JSON object. The message field of the JSON object is
-then returned.
-'''
 def processBrowserLogEntry(entry):
     response = json.loads(entry['message'])['message']
     return response
 
-'''
-This function sets up and returns a web driver for the Chrome browser. It uses the ChromeDriverManager to
-automatically download the appropriate version of the ChromeDriver executable. It also sets the logging preferences
-for performance logs.
-'''
 def setupDriver():
 
     # add performance analysis capabilities
@@ -48,6 +37,7 @@ def setupDriver():
     chromeOptions.add_argument('--disable-application-cache')
     chromeOptions.add_argument('--disable-gpu')
     chromeOptions.add_argument("--disable-dev-shm-usage")
+    chromeOptions.add_argument('--disable-blink-features=AutomationControlled')
 
     driver = webdriver.Chrome(
         ChromeDriverManager().install(),
@@ -58,35 +48,22 @@ def setupDriver():
 
     return driver
 
-'''
-This function takes in two arguments, a web driver driver, and a location string. It finds the search textbox element
-by its 'id' and 'location-typeahead-home-input' and enters the location in it. It then finds the button with text
-'Find Food' using xpath, clicks on it and waits for 5 seconds for the restaurants feed to load.
-'''
-def routeToRestaurantsFeed(driver, location):
+def routeToRestaurantsFeed(fullQueryUrl):
+    driver = setupDriver()
+    driver.get(fullQueryUrl)
+
     searchTextbox = driver.find_element("id","location-typeahead-home-input")
-    searchTextbox.send_keys(location)
-    
+    searchTextbox.send_keys(location)    
     time.sleep(2)
+
     b = driver.find_element(By.XPATH, '//button[text()="Find Food"]')
-
     time.sleep(2)
-    b.click()
 
+    b.click()
     time.sleep(5)
 
-'''
-Retrieves the feed events from the browser performance log of the provided web driver.
+    return driver
 
-Inputs:
-- driver (webdriver): A Selenium webdriver object representing a web browser.
-
-Output:
-- (list of dictionaries): A list of dictionaries containing information about the feed events in the browser performance
-    log. Each dictionary contains the following keys:
-    - method (string): The method of the event.
-    - params (dictionary): A dictionary of parameters related to the event.
-'''
 def getFeedEvents(driver):
     perf = driver.get_log('performance')
     events = [processBrowserLogEntry(entry) for entry in perf]
@@ -101,22 +78,10 @@ def getFeedEvents(driver):
 
     return feedEvents
 
-'''
-This function writes the given object to a file with the specified filename. It opens the file with write mode 'w'
-using the open() function and writes the object to the file by calling json.dump(object, f). The json.dump() function
-serializes the object to a JSON formatted string and writes it to the file. This function can be used to save the
-result of some processing to a file for future use or analysis.
-'''
 def writeToResultFile(object, filename):
     with open(filename, 'w') as f:
         json.dump(object, f)
 
-'''
-gets the names and locations of restaurants from getFeedV1 request
-- data.feedItems.[n].store.mapMarker.latitude
-- data.feedItems.[n].store.mapMarker.longitude
-- data.feedItems.[n].store.title.text
-'''
 def getRestaurantData(feedEvents, category):
     restaurantData = []
     for feedEvent in feedEvents:
@@ -142,12 +107,7 @@ def getRestaurantData(feedEvents, category):
         
     return restaurantData
 
-'''
-This function takes in a list of restaurant data and a web driver as arguments. It iterates through the restaurantData
-list, clicks on the restaurant link, extracts menu data from the menu page, adds it to the restaurant data and goes
-back to the previous page. It then returns the updated restaurant data list with menu information added.
-'''
-def addMenuData(restaurantData, driver):
+def addMenuData(restaurantData, driver, currQueryUrl):
 
     for i in range(0, len(restaurantData)):
 
@@ -158,7 +118,7 @@ def addMenuData(restaurantData, driver):
             continue
         
         restaurantLink.click()
-        time.sleep(4)
+        time.sleep(2)
         
         try:
             spanTexts = [span.text for span in driver.find_elements(By.XPATH, '//span')]
@@ -170,52 +130,50 @@ def addMenuData(restaurantData, driver):
                     menu.append({ 'name': spanTexts[j - 1], 'price': spanTexts[j] })
             if len(menu) > 1: menu.pop(0)
             restaurantData[i]['menu'] = menu
-            print('Got', len(menu), ' items of menu data for', restaurantData[i]['name'])
+            print('Got', len(menu), 'items of menu data for', restaurantData[i]['name'])
         except:
             print('Could not get menu data for', restaurantData[i]['name'])
             pass
         
-        driver.execute_script("window.history.go(-1)")
-        time.sleep(4)
+        driver = resetDriver(driver, currQueryUrl)
+        driver = routeToRestaurantsFeed(currQueryUrl)
+        time.sleep(2)
 
     return restaurantData
 
-'''
-This function takes in a queryUrl and returns the last element of the url. It can be used to extract the category
-from a url.
-'''
 def getCategory(queryUrl):
     return os.path.basename(queryUrl)
 
+def resetDriver(driver, currQueryUrl):
+    driver.close()
+    driver.quit()
+    time.sleep(2)
+
+def getSubQueryUrls(queryUrlsFile):
+    queryUrls = json.load(queryUrlsFile)
+    return queryUrls
+
 if __name__ == "__main__":
 
-    driver = setupDriver()
-    location = 'Pitt Street Mall'
     queryUrlsFile = open('queryUrls.json')
-    queryUrls = json.load(queryUrlsFile)
+    subQueryUrls = getSubQueryUrls(queryUrlsFile);    
 
-    for queryUrl in queryUrls[:2]:
-        category = getCategory(queryUrl)
+    for subQueryUrl in subQueryUrls[:2]:
+        
+        category = getCategory(subQueryUrl)
         print('Getting data for category:', category)
-
-        url = getQueryUrl(category)
-        driver.get(url)
+        fullQueryUrl = getFullQueryUrl(category)
 
         try:
-            routeToRestaurantsFeed(driver, location)
+            driver = routeToRestaurantsFeed(fullQueryUrl)
+            feedEvents = getFeedEvents(driver)
         except:
             print('Could not route to feed for category:', category)
             continue
 
-        feedEvents = getFeedEvents(driver)
-
-        try:
-            restaurantData = addMenuData(getRestaurantData(feedEvents, category), driver)
-        except:
-            pass
-        finally:
-            print('Finished scraping category:', category)
-            writeToResultFile(restaurantData, f'{category}.json')
+        restaurantData = addMenuData(getRestaurantData(feedEvents, category), driver, fullQueryUrl)
+        print('Finished scraping category:', category)
+        writeToResultFile(restaurantData, f'{category}.json')
     
     queryUrlsFile.close()
 
