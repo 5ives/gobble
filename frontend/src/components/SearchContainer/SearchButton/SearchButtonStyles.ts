eeec72dfd637ac26a525a37ccfb35467db53695b
Lorenzo Paras <ivesparas@gmail.com>
2023-02-14 19:21:27 +1100

    [GOB-7] - implement search button

---
+++ frontend/src/components/SearchContainer/SearchButton/SearchButtonStyles.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import { BORDER_RADIUS, HEIGHT, MARGIN, WIDTH } from "../../../consts/spacing";
+
+export const SearchButtonWrapper = styled('div')`
+    margin: ${MARGIN.MEDIUM} 0;
+`;
+
+export const SearchButtonRootStyle = {
+    width: WIDTH.EXTRA_EXTRA_LARGE,
+    height: HEIGHT.EXTRA_LARGE,
+    borderRadius: BORDER_RADIUS.EXTRA_SMALL
+}
