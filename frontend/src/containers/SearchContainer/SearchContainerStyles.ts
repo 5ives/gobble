dd0142c68e70a595f28a0f23288dbaaf50e631a6
Lorenzo Paras <ivesparas@gmail.com>
2023-02-14 19:29:41 +1100

    [GOB-7] - add box styling and add button to filters container

--- frontend/src/containers/SearchContainer/SearchContainerStyles.ts
+++ frontend/src/containers/SearchContainer/SearchContainerStyles.ts
@@ -1,8 +1,17 @@
 import styled from "@emotion/styled";
+import { SEARCH_CONTAINER_BOX_PADDING, SEARCH_CONTAINER_BOX_WIDTH } from "../../consts/component-styles";
 
 export const SearchContainerWrapper = styled('div')`
     width: 100vw;
     height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const SearchContainerBox = styled('div')`
+    width: ${SEARCH_CONTAINER_BOX_WIDTH}px;
+    padding: ${SEARCH_CONTAINER_BOX_PADDING}px;
     display: flex;
     flex-direction: column;
     align-items: center;
