87f205d207076e8ff45c4c78dc84596811076a3d
Lorenzo Paras <ivesparas@gmail.com>
2023-02-14 19:27:53 +1100

    [GOB-7] - create spacing consts and implement in component styles file

---
+++ frontend/src/consts/spacing.ts
@@ -0,0 +1,52 @@
+
+export const MARGIN = {
+    EXTRA_SMALL: '0.5em',
+    SMALL: '1em',
+    SMALL_MEDIUM: '1.5em',
+    MEDIUM: '2em',
+    LARGE: '3em',
+    EXTRA_LARGE: '4em'
+};
+
+export const PADDING = {
+    EXTRA_SMALL: '0.5em',
+    SMALL: '1em',
+    MEDIUM: '2em',
+    LARGE: '3em',
+    EXTRA_LARGE: '4em'
+};
+
+export const BORDER_RADIUS = {
+    EXTRA_SMALL: '0.5em',
+    SMALL: '1em',
+    MEDIUM: '2em',
+    LARGE: '3em',
+    EXTRA_LARGE: '4em',
+    MAX: '9999px'
+};
+
+export const BORDER_SIZE = {
+    EXTRA_SMALL: '0.5em',
+    SMALL: '1em',
+    MEDIUM: '2em',
+    LARGE: '3em',
+    EXTRA_LARGE: '4em'
+};
+
+export const WIDTH = {
+    EXTRA_SMALL: '0.5em',
+    SMALL: '1em',
+    MEDIUM: '2em',
+    LARGE: '3em',
+    EXTRA_LARGE: '4em',
+    EXTRA_EXTRA_LARGE: '30em'
+};
+
+export const HEIGHT = {
+    EXTRA_SMALL: '0.5em',
+    SMALL: '1em',
+    MEDIUM: '2em',
+    LARGE: '3em',
+    EXTRA_LARGE: '4em',
+    EXTRA_EXTRA_LARGE: '14em'
+};
