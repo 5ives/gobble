9d2d84ee04e7607533a101a1c2756de4cc8887c5
Lorenzo Paras <ivesparas@gmail.com>
2023-02-22 18:48:11 +1100

    [GOB-14] - connect isSubmitted search input to button

--- frontend/src/components/SearchContainer/SearchButton/SearchButton.tsx
+++ frontend/src/components/SearchContainer/SearchButton/SearchButton.tsx
@@ -1,14 +1,32 @@
 import { Button } from '@mantine/core';
+import { useContext } from 'react';
+import { CUISINES } from '../../../consts/cuisines';
+import { SearchContext } from '../../../context/useSearchContext/useSearchContext';
+import { ISearchContext, ISearchInput } from '../../../context/useSearchContext/useSearchContextTypes';
 import { SearchButtonRootStyle, SearchButtonWrapper } from './SearchButtonStyles';
 
 const SearchButton = () => {
+    const { searchInput, setSearchInput } = useContext<ISearchContext>(SearchContext);
+
+    const onClick = () => {
+        if(!isSearchInputValid()) return;
+        setSearchInput((searchInput : ISearchInput) => ({ ...searchInput, isSubmitted: true }));
+    };
+
+    const isSearchInputValid = () => {
+        if (!CUISINES.includes(searchInput.cuisine)) return false;
+        if (searchInput.minPrice < 0 || searchInput.maxPrice > 100) return false;
+        return true;
+    };
+
     return (
         <SearchButtonWrapper>
             <Button
                 color='red'
-                styles={(theme) => ({
+                styles={() => ({
                     root: SearchButtonRootStyle
                 })}
+                onClick={() => onClick()}
             >
                 Search
             </Button>
