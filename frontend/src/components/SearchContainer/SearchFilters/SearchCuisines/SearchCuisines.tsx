43dc112038910850d19e51841d93ebb49d2729e4
Lorenzo Paras <ivesparas@gmail.com>
2023-02-22 18:44:26 +1100

    [GOB-14] - connect cuisine search input to search cuisines component

--- frontend/src/components/SearchContainer/SearchFilters/SearchCuisines/SearchCuisines.tsx
+++ frontend/src/components/SearchContainer/SearchFilters/SearchCuisines/SearchCuisines.tsx
@@ -1,13 +1,22 @@
+import { useContext } from "react";
+import { SearchContext } from "../../../../context/useSearchContext/useSearchContext";
+import { ISearchContext, ISearchInput } from "../../../../context/useSearchContext/useSearchContextTypes";
 import Heading from "../../../common/Heading/Heading";
 import SearchCuisineBadges from "./SearchCuisinesBadges/SearchCuisinesBadges";
 import SearchCuisinesInput from "./SearchCuisinesInput/SearchCuisinesInput";
 
 const SearchCuisines = () => {
+    const { searchInput, setSearchInput } = useContext<ISearchContext>(SearchContext);
+
+    const handleCuisineChange = (newCuisine : string) => {
+        setSearchInput((searchInput : ISearchInput) => ({ ...searchInput, cuisine: newCuisine }))
+    };
+
     return (
         <>   
             <Heading>Cuisines</Heading>
-            <SearchCuisinesInput/>
-            <SearchCuisineBadges/>
+            <SearchCuisinesInput value={searchInput.cuisine}/>
+            <SearchCuisineBadges onClick={handleCuisineChange}/>
         </>
     );
 };
--- frontend/src/components/SearchContainer/SearchFilters/SearchCuisines/SearchCuisinesBadge/SearchCuisinesBadge.tsx
+++ frontend/src/components/SearchContainer/SearchFilters/SearchCuisines/SearchCuisinesBadge/SearchCuisinesBadge.tsx
@@ -1,9 +1,11 @@
 import { Badge } from "@mantine/core";
 import { SearchCuisinesBadgeWrapper } from "./SearchCuisinesBadgeStyles";
 
-const CuisineBadge = ({ cuisine } : { cuisine : string }) => {
+const CuisineBadge = ({ cuisine, onClick } : { cuisine : string, onClick : Function }) => {
     return (
-        <SearchCuisinesBadgeWrapper><Badge size="xl" color="gray">{cuisine}</Badge></SearchCuisinesBadgeWrapper>
+        <SearchCuisinesBadgeWrapper>
+            <Badge size="xl" color="gray" onClick={() => onClick(cuisine)}>{cuisine}</Badge>
+        </SearchCuisinesBadgeWrapper>
     );
 };
 
