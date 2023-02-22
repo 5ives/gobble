45460d076835e3a7faf99e434cbbe5d383c53876
Lorenzo Paras <ivesparas@gmail.com>
2023-02-22 18:48:32 +1100

    [GOB-14] - base container switch depending on isSubmitted

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,16 @@ import { useState } from 'react';
 import MapContainer from './containers/MapContainer/MapContainer';
 import SearchContainer from './containers/SearchContainer/SearchContainer';
 import { SearchContext, defaultSearchInput } from './context/useSearchContext/useSearchContext';
-import { DEFAULT_MAX_PRICE, DEFAULT_MIN_PRICE } from './context/useSearchContext/useSearchContextConsts';
 import { ISearchInput } from './context/useSearchContext/useSearchContextTypes';
 
 const App = () => {
 	const [searchInput, setSearchInput] = useState<ISearchInput>(defaultSearchInput)
 	const searchInputValue = { searchInput, setSearchInput };
+
 	return (
 		<SearchContext.Provider value={searchInputValue}>
 			<div className="App">
-				{
-					searchInput.food === '' ||
-					searchInput.maxPrice === DEFAULT_MAX_PRICE ||
-					searchInput.minPrice === DEFAULT_MIN_PRICE
-						? <SearchContainer/> 
-						: <MapContainer />
-				}
+				{ searchInput.isSubmitted ? <MapContainer /> : <SearchContainer /> }
 			</div>
 		</SearchContext.Provider>
 	);
