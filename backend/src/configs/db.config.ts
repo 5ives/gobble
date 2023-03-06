0ca872ad5fef0f38a86bb59f566a47da64a309b0
Lorenzo Paras <ivesparas@gmail.com>
2023-03-06 18:15:28 +1100

    [GOB-18] - setup DB connection

---
+++ backend/src/configs/db.config.ts
@@ -0,0 +1,13 @@
+
+const config = {
+    user: 'lorenzoparas',
+    password: 'NJvmaQ4GHXsDRB9tb5UD',
+    database: 'gobble_db',
+    host: 'gobble-db.c8xnamihzakf.ap-southeast-2.rds.amazonaws.com',
+    port: 5432,
+    statement_timeout: 50000,
+    query_timeout: 50000,
+    application_name: 'gobble-backend'
+};
+
+export default config;
