3818df136dba8dc6760c62db990fd72bb70cf275
Lorenzo Paras <ivesparas@gmail.com>
2023-03-08 20:01:33 +1100

    [GOB-19] - fix api url

--- frontend/src/services/RestaurantService.ts
+++ frontend/src/services/RestaurantService.ts
@@ -1,4 +1,6 @@
 import { Filters } from "../types/filters.type";
 import { getRequest } from "../utils/apiUtils";
 
-export const getRestaurants = async (filters: Filters) => await getRequest('http://localhost:8080/api/v1/restaurants', filters);
+export const getRestaurants = async (filters: Filters) => {
+    await getRequest('http://gobble-api.eba-y3hkh3fb.ap-southeast-2.elasticbeanstalk.com/api/v1/restaurants', filters);
+};
