d38d615c02ad9b945ed510b6c62c51a4ea478c18
Lorenzo Paras <ivesparas@gmail.com>
2023-03-08 16:44:10 +1100

    [GOB-19] - fix console log and add missing req argument make it no used

--- backend/index.ts
+++ backend/index.ts
@@ -6,11 +6,11 @@ import * as cors from 'cors';
 const app = express();
 const port : Number = 80;
   
-app.get('/', (res: Response) => {
+app.get('/', (_req: Request, res: Response) => {
     res.send('Welcome to the gobble backend!');
 })
   
-app.listen(port,() => console.log('The application is listening on port' + port));
+app.listen(port,() => console.log('The application is listening on port ' + port));
 
 app.use(cors({ origin: ['http://localhost:3000'] } as cors.CorsOptions));
 app.use('/api/v1', restaurantRouter);
