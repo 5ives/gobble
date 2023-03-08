65d6280f55d44a215c2015bcf5428656664c6fe5
Lorenzo Paras <ivesparas@gmail.com>
2023-03-08 20:05:50 +1100

    [GOB-19] - change from http to https

--- frontend/src/services/RestaurantService.ts
+++ frontend/src/services/RestaurantService.ts
@@ -2,5 +2,5 @@ import { Filters } from "../types/filters.type";
 import { getRequest } from "../utils/apiUtils";
 
 export const getRestaurants = async (filters: Filters) => {
-    await getRequest('http://gobble-api.eba-y3hkh3fb.ap-southeast-2.elasticbeanstalk.com/api/v1/restaurants', filters);
+    await getRequest('https://gobble-api.eba-y3hkh3fb.ap-southeast-2.elasticbeanstalk.com/api/v1/restaurants', filters);
 };
