514d2ab2583fe1eb4eadfcb1108919b46b4235e9
Lorenzo Paras <ivesparas@gmail.com>
2023-05-09 20:58:25 +1000

    [GOB-39] - remove EB URL

--- frontend/src/services/RestaurantService.ts
+++ frontend/src/services/RestaurantService.ts
@@ -2,6 +2,6 @@ import { Filters } from "../types/filters.type";
 import { getRequest } from "../utils/apiUtils";
 
 export const getRestaurants = async (filters: Filters) => {
-    const restaurants = await getRequest('http://gobble-api.eba-y3hkh3fb.ap-southeast-2.elasticbeanstalk.com/api/v1/restaurants', filters);
+    const restaurants = await getRequest('http://localhost:8080/api/v1/restaurants', filters);
     return restaurants;
 };
